27321a606581768a38c04f5f5cbb1044e10c98ee
Lynt <strawww@gmail.com>
2017-04-10 11:34:20 +1000

    add component with state

---
+++ src/SelectState.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import wrapWithClickout from 'react-clickout';
+import SelectComponent, { propTypes, defaultProps } from './SelectComponent';
+
+class Select extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+      options: props.options,
+    };
+  }
+
+  componentDidMount() {
+    this.state = {
+      isOpen: false,
+      options: this.props.options,
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.options) {
+      this.setState({
+        options: nextProps.options,
+      });
+    }
+  }
+
+  toggleOpen = () => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+
+  handleClickout = () => {
+    this.setState({
+      isOpen: false,
+    });
+  }
+
+  render() {
+    return (
+      <SelectComponent
+        identifier={this.props.identifier}
+        label={this.props.label}
+        options={this.state.options}
+        isOpen={this.state.isOpen}
+        isMultipleSelect={this.props.isMultipleSelect}
+        isSearchable={this.props.isSearchable}
+        toggleOpen={this.toggleOpen}
+        onChange={this.props.onChange}
+        onSearch={this.props.onSearch}
+        styles={this.props.styles}
+      />
+    );
+  }
+}
+
+Select.propTypes = {
+  identifier: propTypes.identifier,
+  label: propTypes.label,
+  isMultipleSelect: propTypes.isMultipleSelect,
+  isSearchable: propTypes.isMultipleSelect,
+  onChange: propTypes.onChange,
+  onSearch: propTypes.onSearch,
+  options: propTypes.options,
+  styles: propTypes.styles,
+};
+
+Select.defaultProps = defaultProps;
+
+export default wrapWithClickout(Select);
