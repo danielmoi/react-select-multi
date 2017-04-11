478899e5992c8208b1b06cc1b4dcb318e532e956
Lynt <strawww@gmail.com>
2017-04-11 16:33:42 +1000

    update the redux for addSet

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -4,22 +4,29 @@ import initialState from './initial_state';
 
 export default (state = fromJS(initialState), action) => {
   switch (action.type) {
+    case C.SELECT_ADD_SET: {
+      const { name } = action.data;
+      if (!name) return state;
+      return state.merge({
+        [name]: { isOpen: false, selected: [] },
+      });
+    }
     case C.SELECT_TOGGLE_OPEN: {
       const { name, open } = action.data;
       return state.merge({
-        [name]: Object.assign(state.get(name) || {}, { isOpen: open }),
+        [name]: (state.get(name) || fromJS({})).merge({ isOpen: open }),
       });
     }
     case C.SELECT_SEARCH_VALUES: {
       const { name, searchText } = action.data;
       return state.merge({
-        [name]: Object.assign(state.get[name] || {}, { searchTerm: searchText }),
+        [name]: (state.get(name) || fromJS({})).merge({ searchTerm: searchText }),
       });
     }
     case C.SELECT_TAKE_VALUE: {
       const { name, values } = action.data;
       return state.merge({
-        [name]: Object.assign(state.get[name] || {}, { values }),
+        [name]: (state.get(name) || fromJS({})).merge({ selected: values }),
       });
     }
     case C.SELECT_CLEAR_ALL: {
