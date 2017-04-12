21ba844b7e5e6eebfc21d9cd58428b11c6d47ed3
danielmoi <daniel@ampersandmoi.com>
2017-04-12 17:57:42 +1000

    setup

---
+++ test/fixtures/index.js
@@ -0,0 +1,6 @@
+export const options = [
+  { display: 'Hotdogs', tag: 'hotdogs' },
+  { display: 'Buns', tag: 'buns' },
+  { display: 'Wigs', tag: 'wigs' },
+  { display: 'Shoelaces', tag: 'shoelaces' },
+];
