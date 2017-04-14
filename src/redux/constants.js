6d4065449fd59633918ad014024e215ceba12bb3
danielmoi <daniel@ampersandmoi.com>
2017-04-15 09:29:46 +1000

    simplify actions

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,9 +1,16 @@
-// TODO: Add convenience helper
-export default {
-  RSM_ADD_SELECT: 'RSM_ADD_SELECT',
-  RSM_TOGGLE_OPEN: 'RSM_TOGGLE_OPEN',
-  RSM_HANDLE_CLICKOUT: 'RSM_HANDLE_CLICKOUT',
-  RSM_SEARCH_OPTIONS: 'RSM_SEARCH_OPTIONS',
-  RSM_SAVE_SELECTED: 'RSM_SAVE_SELECTED',
-  RSM_REMOVE_SELECT: 'RSM_REMOVE_SELECT',
-};
+const ACTIONS = [
+  'RSM_ADD_SELECT',
+  'RSM_TOGGLE_OPEN',
+  'RSM_HANDLE_CLICKOUT',
+  'RSM_SEARCH_OPTIONS',
+  'RSM_SAVE_SELECTED',
+  'RSM_REMOVE_SELECT',
+];
+
+const populatedActions = {};
+
+for (const action of ACTIONS) {
+  populatedActions[action] = action;
+}
+
+export default populatedActions;
