1f5455198d7d03b934fe8082c90835f4f1a43fad
Anoop Varghese <anupvarghese@gmail.com>
2017-04-14 20:04:10 +1000

    types

--- src/types/index.js
+++ src/types/index.js
@@ -1,15 +1,117 @@
 /* @flow */
 
-import type { Map } from 'immutable';
+import type {
+  Map,
+  List,
+} from 'immutable';
 
 export type Action = {
-  type: String,
+  type: string,
   data: Object,
 };
 
 export type FSA = (any) => Action;
 
-export type DataState = Map<string, ?any>;
+export type DataState = Map < string, ? any > ;
 
 export type Reducer = (state: DataState, action: Action) => DataState;
 
+export type Callback = (any) => any;
+
+export type Option = {
+  tag: string,
+  display: string,
+};
+
+export type SelectBaseProps = {
+  uniqueKey: string,
+  label: string,
+  options: Array < Option > | List < Option >,
+  selected: Array < string > | List < string > | Map < string,
+  ?
+  any >,
+  searchTerm ? : string,
+  isMultipleSelect : boolean,
+  isSearchable: boolean,
+  isOpen: boolean,
+  toggleOpen: Callback,
+  onCheck: Callback,
+  onSearch: ? Callback,
+  placeholder: string,
+  styles: Object,
+  name ? : string,
+  defaultValues ? : Array < string >,
+  taskeValue ? : Callback,
+};
+
+export type SelectStateProps = {
+  uniqueKey: string,
+  label: string,
+  options: Array < Option > | List < Option >,
+  selected: Array < string > | List < string > | Map < string,
+  ?
+  any >,
+  searchTerm ? : string,
+  isMultipleSelect : boolean,
+  isSearchable: boolean,
+  isOpen: boolean,
+  toggleOpen: Callback,
+  onCheck: Callback,
+  onSearch: ? Callback,
+  placeholder: string,
+  styles: Object,
+  name ? : string,
+  defaultValues ? : Array < string >,
+  taskeValue ? : Callback,
+};
+
+export type SelectConnectedProps = {
+  uniqueKey: string,
+  label: string,
+  options: Array < Option > | List < Option >,
+  selected: Array < string > | List < string > | Map < string,
+  ?
+  any >,
+  searchTerm ? : string,
+  isMultipleSelect : boolean,
+  isSearchable: boolean,
+  isOpen: boolean,
+  toggleOpen: Callback,
+  onCheck: Callback,
+  onSearch: ? Callback,
+  placeholder: string,
+  styles: Object,
+  name: string,
+  defaultValues: Array < string >,
+  takeValue: Callback,
+  addSet: Callback,
+  clearAll: Callback,
+};
+
+export type SelectConnectedDefaultProps = {
+  label: string,
+  searchTerm ? : string,
+  isMultipleSelect : boolean,
+  isSearchable: boolean,
+  isOpen: boolean,
+  onSearch: ? Callback,
+  placeholder: string,
+  styles: Object,
+};
+
+export type SelectStateDefaultProps = {
+  label: string,
+  searchTerm?: string,
+  isMultipleSelect: boolean,
+  isSearchable: boolean,
+  isOpen: boolean,
+  onSearch: ? Callback,
+  placeholder: string,
+  styles: Object,
+};
+
+
+export type SelectState = {
+  isOpen: boolean,
+  options: Array < Option > | List < Option >,
+};
