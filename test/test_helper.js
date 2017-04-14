8252a98b9dd95faabc5225028526d79497c8a086
danielmoi <daniel@ampersandmoi.com>
2017-04-14 17:38:59 +1000

    fix test_helper for unmount

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,24 +1,20 @@
-/*eslint-disable*/
-// var shallowWithIntl = require('./helpers/intl-enzyme-test-helper').shallowWithIntl;
-// var mountWithIntl = require('./helpers/intl-enzyme-test-helper').mountWithIntl;
 require('babel-register')();
-var chai = require('chai');
-// var chaiImmutable = require('chai-immutable');
-var jsdom = require('jsdom').jsdom;
-const chaiEnzyme = require('chai-enzyme');
-var exposedProperties = ['window', 'navigator', 'document'];
+const chai = require('chai');
+const { jsdom } = require('jsdom');
 
+const exposedProperties = ['window', 'navigator', 'document'];
 
-require.extensions['.scss'] = () => {
-  return;
-};
-require.extensions['.css'] = () => {
-  return;
-};
-global.document = jsdom('');
+// This prevents node/babel from handling style requires like var style = require('./style.scss');
+// https://github.com/react-toolbox/react-toolbox/issues/633#issuecomment-235087142
+require.extensions['.scss'] = () => {};
+require.extensions['.css'] = () => {};
+
+global.document = jsdom('<body></body>');
 global.window = document.defaultView;
-// global.shallowWithIntl = shallowWithIntl;
-// global.mountWithIntl = mountWithIntl;
+global.navigator = {
+  userAgent: 'node.js'
+};
+
 Object.keys(document.defaultView).forEach((property) => {
   if (typeof global[property] === 'undefined') {
     exposedProperties.push(property);
@@ -26,9 +22,10 @@ Object.keys(document.defaultView).forEach((property) => {
   }
 });
 
-global.navigator = {
-  userAgent: 'node.js'
-};
 
-// chai.use(chaiImmutable);
-chai.use(chaiEnzyme());
+// this has to happen after the globals are setup
+// the require flow is: chai-enzyme > enzyme > react > fbjs/lib/ExecutionEnvironment
+// so, we need to initialize globals before requiring chai-enzyme
+// this allows .unmount() to work
+// https://github.com/airbnb/enzyme/issues/395#issuecomment-239352075
+chai.use(require('chai-enzyme')());
